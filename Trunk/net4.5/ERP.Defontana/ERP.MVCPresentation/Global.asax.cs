e2feb620052706223f75f64e5da5f1837d13789b
lvasquezdf <lvasquez@defontana.com>
2015-02-18 16:58:45 -0300

    Levantamiento inicial de esqueleto proyecto Spartan ERP Defontana

---
+++ Trunk/net4.5/ERP.Defontana/ERP.MVCPresentation/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using log4net;
+using log4net.Config;
+using ERP.Utilities.Log;
+
+namespace ERP.Presentation
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        //protected static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected void Application_Start()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Error(Object sender, System.EventArgs e)
+        {
+            //Registramos errores de Aplicaciones en el Log4net.
+            HttpContext context = HttpContext.Current;
+            Exception ex = Server.GetLastError();
+            ErrorCatch.Save(ex);
+
+
+
+                
+            //Log.Error(ex);
+
+            
+
+        }
+    }
+}
---
+++ Trunk/net4.5/ERP.Defontana/ERP.Utilities/Log/ErrorCatch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using log4net;
+
+namespace ERP.Utilities.Log
+{
+    public static class ErrorCatch
+    { 
+        
+        public  static void Save(Exception ex) {
+                Log.Error("Error:", ex);
+        }
+
+        //static readonly log4net.ILog Log = log4net.LogManager.GetLogger(typeof(ErrorCatch));          
+        static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    }      
+}
