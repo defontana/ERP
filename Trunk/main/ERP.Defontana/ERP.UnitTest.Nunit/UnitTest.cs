33fb43ac98552bc3486f9c1ed68c4f448ff5b10a
lvasquezdf <lvasquez@defontana.com>
2015-02-26 15:58:21 -0300

    Inclusion de Test Nunit en proyecto.

---
+++ Trunk/main/ERP.Defontana/ERP.UnitTest.Nunit/UnitTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ERP.UnitTest.Nunit
+{
+    [TestFixture] // El atributo TestFixture identifica a esta clase como una clase de pruebas
+    public class UnitTest
+    {
+
+        [Test] // El atributo Test identifica a este método como una prueba unitaria
+        public void TestPruebaAssertBamboo()
+        {
+            string texto = "Hola Mundo";
+            Assert.AreNotSame(texto.Length, 0);
+        }
+
+        [Test]
+        public void TestPruebaErrorBamboo()
+        {
+            int divisor = 0;
+
+            double valor = 5 / divisor;
+            Assert.AreNotEqual(valor, 0);
+        }
+
+        [Test]
+        public void TestPruebaNotAssertBamboo()
+        {
+            int divisor = 0;
+
+            Assert.AreNotEqual(0, divisor);
+        }
+    }
+}
+
+
