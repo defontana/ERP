3fb170a4d7e530527c17c85bd47fe4b2e725f60e
lvasquezdf <lvasquez@defontana.com>
2015-02-26 11:29:22 -0300

    Prueba de Test
    3 Test
    1 assert ok
    2 error sintaxtico
    3 no assert.

--- Trunk/main/ERP.Defontana/ERP.UnitTest/ClientUnitTest.cs
+++ Trunk/main/ERP.Defontana/ERP.UnitTest/ClientUnitTest.cs
@@ -3,12 +3,36 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ERP.UnitTest
 {
+
+    /// <summary>
+    /// Prueba fija para comprobar Test con Bamboo
+    /// </summary>
     [TestClass]
     public class ClientUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        
+        public void TestPruebaAssertBamboo()
         {
+            string texto = "Hola Mundo";
+            Assert.AreNotSame(texto.Length,0);
+        }
+
+        [TestMethod]
+        public void TestPruebaErrorBamboo()
+        {
+            int divisor = 0;
+
+            double valor = 5 / divisor;
+            Assert.AreNotEqual(valor, 0);
+        }
+
+        [TestMethod]
+        public void TestPruebaNotAssertBamboo()
+        {
+            int divisor = 0;
+
+            Assert.AreNotEqual(0, divisor);
         }
     }
 }
